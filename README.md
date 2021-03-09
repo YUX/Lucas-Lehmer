2393d56b779f7b3db8ab6b6536d8fa1acfbca169
Yu XIAO <yu.xiao.fr@gmail.com>
2021-03-09 14:03:34 +0800

    Further Optimizations

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ An implementation of the [Lucas-Lehmer](https://en.wikipedia.org/wiki/Lucas–Le
 ### Optimizations
  * For all Mersenne primes (of the form 2<sup>p</sup>-1) p has to be a prime number, so before doing the more costly Lucas-lehmer test we make sure p is prime.
  * The modulus in the Lucas-Lehmer test is made a lot faster in the fastMod function which uses bitwise operators instead of division, moving the slowest part of the test to the multiplication of s*s.
+
+### Further Optimizations
+- replace `import "math/big"` with `import big "github.com/ncw/gmp"` ends up 8+ times speedup.
+- log to file
+- use the built-in `func (*Int) ProbablyPrime`  
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"math/big"
+	"log"
+	"os"
+	"runtime"
 	"sync"
+	"time"
+
+	big "github.com/ncw/gmp"
+	// "github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -13,86 +18,73 @@ var (
 	two  *big.Int = big.NewInt(2)
 
 	mu         sync.Mutex
-	numWorkers = 4
-	jobs       = make(chan uint, numWorkers)
-
-	smallPrimes = [...]uint{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101}
+	numWorkers = runtime.NumCPU()
+	// numWorkers = 1
 
 	currentP = uint(1)
 )
 
 const (
-	iterationsPerJob = 128
+	iterationsPerJob = 2048
 )
-
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
 	fmt.Println("Starting to look for primes")
+	fmt.Println("numWorkers:", numWorkers)
+	fmt.Println(time.Now())
+
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.SetOutput(file)
 
 	wg := &sync.WaitGroup{}
 
 	for i := 0; i < numWorkers; i++ {
 		wg.Add(1)
-		go spawnWorker(wg)
+		go spawnWorker()
 	}
 
-	// We neved call wg.Done, so we'll just keep searching
 	wg.Wait()
 }
 
-func spawnWorker(wg *sync.WaitGroup) {
+func spawnWorker() {
 	for {
-		// Reserve some primes for us to work with
 		mu.Lock()
 		initialP := currentP
 		currentP += iterationsPerJob
 		mu.Unlock()
 
+		// bar := pb.StartNew(iterationsPerJob)
+
 		for i := uint(0); i < iterationsPerJob; i++ {
 			myP := initialP + i
-			// Little bit of initial factoring because Lucas-Lehmer is expensive
-			mightBePrime := true
-			for j := uint(0); j < uint(len(smallPrimes)); j++ {
-				if myP%smallPrimes[j] == 0 {
-					mightBePrime = false
-					break
-				}
-			}
-
-			if mightBePrime && LucasLehmer(myP) {
+			if big.NewInt(int64(myP)).ProbablyPrime(4) && LucasLehmer(myP) {
 				fmt.Printf("2^%d-1 is prime\n", myP)
+				log.Println("2^", myP, "-1 is prime")
+				// fmt.Println(time.Now())
 			}
+			// bar.Increment()
 		}
 	}
 }
 
-// p here is the same p as in 2^p-1
 func LucasLehmer(p uint) (isPrime bool) {
 	var dummy1, dummy2 big.Int
-
 	s := big.NewInt(4)
 	m := big.NewInt(0)
 	m = m.Sub(m.Lsh(one, p), one) // = (1 << p) - 1
 
 	for i := 0; i < int(p)-2; i++ {
-		// This is the slower but straightforward way
-		//s.Mod(s.Sub(s.Mul(s, s), two), m)
-
-		// Or, use this faster way
-		// s here is the same as k in this part of the wikipedia page:
-		// https://en.wikipedia.org/wiki/Lucas%E2%80%93Lehmer_primality_test#Time_complexity
 		s = s.Sub(s.Mul(s, s), two)
-
 		for s.Cmp(m) == 1 {
-			// And is big's logical and, Rsh is right shift
 			s.Add(dummy1.And(s, m), dummy2.Rsh(s, p))
 		}
-
 		if s.Cmp(m) == 0 {
 			s = zero
 		}
 	}
-
 	return s.Cmp(zero) == 0
 }
